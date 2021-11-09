725e1a2b8f2c3c2164e07a5d98e2f7d88ded1eac
Anders Halvgaard <007744@dtu.dk>
2021-11-09 15:11:28 +0100

    Commit 12 - Klar til Gui modifikation.

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -1,8 +1,18 @@
 package com.company;
-import gui_main.GUI;
 
-    public class Main {
-        public static void main(String[] args) {
-            GUI gui = new GUI();
-        }
+import gui_main.GUI;
+//Matadorspilleplade fra Maven - Starter GUI herfra Main.
+
+public class Main {
+
+    public static void main(String[] args) {
+        GUI gui = new GUI();
+
+        // Opretter spiller med navnet Junior og start balance på 2000
+        //GUI_Player player = new GUI_Player("Junior", 2000);
+
+        // Tilføjer ham til spillet
+        //gui.addPlayer(player);
+
     }
+}
