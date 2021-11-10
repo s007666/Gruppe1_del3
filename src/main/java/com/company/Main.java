58dae67e6fb2edb4044540c78d1a4eb42aae25b5
Anders Halvgaard <007744@dtu.dk>
2021-11-10 13:31:46 +0100

    Commit 12A - Klar til Gui modifikation.

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -1,6 +1,10 @@
 package com.company;
 
+import gui_fields.GUI_Car;
+import gui_fields.GUI_Player;
 import gui_main.GUI;
+
+import java.awt.*;
 //Matadorspilleplade fra Maven - Starter GUI herfra Main.
 
 public class Main {
@@ -9,10 +13,17 @@ public class Main {
         GUI gui = new GUI();
 
         // Opretter spiller med navnet Junior og start balance på 2000
-        //GUI_Player player = new GUI_Player("Junior", 2000);
+        //GUI_Player player = new GUI_Player("Junior", 2000)
+        GUI_Car car1 = new GUI_Car(Color.BLUE, Color.BLACK, GUI_Car.Type.CAR, GUI_Car.Pattern.CHECKERED);
+        GUI_Car car2 = new GUI_Car(Color.RED, Color.WHITE, GUI_Car.Type.RACECAR, GUI_Car.Pattern.CHECKERED);
+        GUI_Player Junior = new GUI_Player("Junior", 2000, car1);
+        GUI_Player Matador = new GUI_Player("Matador",2000, car2);
 
         // Tilføjer ham til spillet
-        //gui.addPlayer(player);
+        gui.addPlayer(Junior);
+        gui.addPlayer(Matador);
+
 
     }
+
 }
