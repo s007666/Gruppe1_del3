12582823d96fc51020025cf65a86218dd10bb262
jasminclemmensen <s215726@dtu.dk>
2021-11-11 14:58:38 +0100

    Commit 17, tilføjelse af terninger

--- src/main/java/com/company/NySpilleplade.java
+++ src/main/java/com/company/NySpilleplade.java
@@ -239,10 +239,17 @@ public class NySpilleplade {
         gui.setDice(1, 2); // Viser to terninger med værdierne 1 og 2
 
 
+        while(true){
+            // Tag i mod input fra brugeren
+            String choice = gui.getUserButtonPressed("Tag et valg", "Slå med én terning", "Slå med to terninger");
+            if( choice.equals("Slå med én terning") )
+                gui.setDie(6); // Vis en terning med værdien 6
+            if( choice.equals("Slå med to terninger") )
+                gui.setDice(1, 2); // Vis to terninger med værdierne 1 og 2
 
 
     }
-}
+}}
 
 
 
