3619aa022fd2aaa99f7502f567b8356ec67326ab
jasminclemmensen <s215726@dtu.dk>
2021-11-11 11:13:45 +0100

    Commit 16, færdig spilleplade

--- src/main/java/com/company/NySpilleplade.java
+++ src/main/java/com/company/NySpilleplade.java
@@ -5,43 +5,212 @@ import gui_fields.*;
 import gui_main.GUI;
 
 import java.awt.*;
+import java.util.Random;
 
 public class NySpilleplade {
-
     public static void main(String[] args) {
+        // Laver hvert felt svarende til spillepladen
+        GUI_Field[] fields = {
+                new GUI_Start(),
+                new GUI_Street(),
+                new GUI_Street(),
+                new GUI_Chance(),
+                new GUI_Street(),
+                new GUI_Street(),
+                new GUI_Jail(),
+                new GUI_Street(),
+                new GUI_Street(),
+                new GUI_Chance(),
+                new GUI_Street(),
+                new GUI_Street(),
+                new GUI_Tax(),
+                new GUI_Street(),
+                new GUI_Street(),
+                new GUI_Chance(),
+                new GUI_Street(),
+                new GUI_Street(),
+                new GUI_Tax(),
+                new GUI_Street(),
+                new GUI_Street(),
+                new GUI_Chance(),
+                new GUI_Street(),
+                new GUI_Street(),
 
-        // Laver array
-        GUI_Field[] fields = new GUI_Field[24];
-
-        // Laver startfelt
-        fields[0] = new GUI_Start("Start", "", "Her starter du", Color.ORANGE, Color.WHITE);
-
-        // Fylder resten af feltenre ud med veje
-        for( int i=1; i<24; i++){
-            if( i%2 == 0 )
-                fields[i] = new GUI_Street(
-                        "Street " + i,
-                        "Price: " + (i*50),
-                        "This is a street",
-                        "100",
-                        Color.PINK,
-                        Color.WHITE
-                );
-            else
-                fields[i] = new GUI_Street(
-                        "Street " + i,
-                        "Price: " + (i*100),
-                        "This is a street",
-                        "50",
-                        Color.BLUE,
-                        Color.WHITE
-                );
-        }
+        };
 
         GUI gui = new GUI(fields);
-    }
+        //ændre beskrivelserne til hvert felt
+        // start feltet
+        gui.getFields()[0].setTitle("START");
+        gui.getFields()[0].setSubText("");
+        gui.getFields()[0].setForeGroundColor(Color.BLACK);
+        gui.getFields()[0].setBackGroundColor(Color.LIGHT_GRAY);
+        gui.getFields()[0].setDescription("Modtag M2, når du passerer");
 
+        //felt 1
+        gui.getFields()[1].setTitle("Burgerbaren");
+        gui.getFields()[1].setSubText("M1");
+        gui.getFields()[1].setForeGroundColor(Color.BLACK);
+        gui.getFields()[1].setBackGroundColor(Color.GREEN);
+        gui.getFields()[1].setDescription("Du landede på burgerbaren");
+
+        //felt 2
+        gui.getFields()[2].setTitle("Pizzahuset");
+        gui.getFields()[2].setSubText("M1");
+        gui.getFields()[2].setForeGroundColor(Color.BLACK);
+        gui.getFields()[2].setBackGroundColor(Color.GREEN);
+        gui.getFields()[2].setDescription("Du landede på pizzahuset");
+        //felt 3
+        gui.getFields()[3].setTitle("Prøv Lykken");
+        gui.getFields()[3].setSubText("");
+        gui.getFields()[3].setForeGroundColor(Color.BLACK);
+        gui.getFields()[3].setBackGroundColor(Color.ORANGE);
+        gui.getFields()[3].setDescription("Prøv lykken kort");
+
+        //felt 4
+        gui.getFields()[4].setTitle("Slikbutikken");
+        gui.getFields()[4].setSubText("M1");
+        gui.getFields()[4].setForeGroundColor(Color.BLACK);
+        gui.getFields()[4].setBackGroundColor(Color.BLUE);
+        gui.getFields()[1].setDescription("Du landede på slikbutikken");
+
+        //felt 5
+        gui.getFields()[5].setTitle("Iskiosken");
+        gui.getFields()[5].setSubText("M1");
+        gui.getFields()[5].setForeGroundColor(Color.BLACK);
+        gui.getFields()[5].setBackGroundColor(Color.BLUE);
+        gui.getFields()[5].setDescription("Du landede på iskiosken");
+
+        // felt 6
+        gui.getFields()[6].setTitle("I fængsel");
+        gui.getFields()[6].setSubText("Bare på besøg");
+        gui.getFields()[6].setForeGroundColor(Color.BLACK);
+        gui.getFields()[6].setBackGroundColor(Color.LIGHT_GRAY);
+        gui.getFields()[6].setDescription("Du på besøg i fængslet");
+
+        //felt 7
+        gui.getFields()[7].setTitle("Museet");
+        gui.getFields()[7].setSubText("M2");
+        gui.getFields()[7].setForeGroundColor(Color.BLACK);
+        gui.getFields()[7].setBackGroundColor(Color.RED);
+        gui.getFields()[7].setDescription("Du landede på museet");
+
+        //felt 8
+        gui.getFields()[8].setTitle("Biblioteket");
+        gui.getFields()[8].setSubText("M2");
+        gui.getFields()[8].setForeGroundColor(Color.BLACK);
+        gui.getFields()[8].setBackGroundColor(Color.RED);
+        gui.getFields()[8].setDescription("Du landede på biblioteket");
+
+        //felt 9
+        gui.getFields()[9].setTitle("Prøv Lykken");
+        gui.getFields()[9].setSubText("");
+        gui.getFields()[9].setForeGroundColor(Color.BLACK);
+        gui.getFields()[9].setBackGroundColor(Color.ORANGE);
+        gui.getFields()[9].setDescription("Prøv lykken kort");
+
+        //felt 10
+        gui.getFields()[10].setTitle("Skaterparken");
+        gui.getFields()[10].setSubText("M2");
+        gui.getFields()[10].setForeGroundColor(Color.BLACK);
+        gui.getFields()[10].setBackGroundColor(Color.YELLOW);
+        gui.getFields()[10].setDescription("Du landede på skaterparken");
+
+        //felt 11
+        gui.getFields()[11].setTitle("Swimmingpoolen");
+        gui.getFields()[11].setSubText("M2");
+        gui.getFields()[11].setForeGroundColor(Color.BLACK);
+        gui.getFields()[11].setBackGroundColor(Color.YELLOW);
+        gui.getFields()[11].setDescription("Du landede på Swimmingpoolen");
+
+        //felt 12
+        gui.getFields()[12].setTitle("Gratis parkering");
+        gui.getFields()[12].setSubText("");
+        gui.getFields()[12].setForeGroundColor(Color.BLACK);
+        gui.getFields()[12].setBackGroundColor(Color.LIGHT_GRAY);
+        gui.getFields()[12].setDescription("Gratis parkering");
+
+        //felt 13
+        gui.getFields()[13].setTitle("Spillehallen");
+        gui.getFields()[13].setSubText("M3");
+        gui.getFields()[13].setForeGroundColor(Color.BLACK);
+        gui.getFields()[13].setBackGroundColor(Color.PINK);
+        gui.getFields()[13].setDescription("Du landede på spillehallen");
+
+        //felt 14
+        gui.getFields()[14].setTitle("Biografen");
+        gui.getFields()[14].setSubText("M3");
+        gui.getFields()[14].setForeGroundColor(Color.BLACK);
+        gui.getFields()[14].setBackGroundColor(Color.PINK);
+        gui.getFields()[14].setDescription("Du landede på biografen");
+
+        //felt 15
+        gui.getFields()[15].setTitle("Prøv Lykken");
+        gui.getFields()[15].setSubText("");
+        gui.getFields()[15].setForeGroundColor(Color.BLACK);
+        gui.getFields()[15].setBackGroundColor(Color.ORANGE);
+        gui.getFields()[15].setDescription("Prøv lykken kort");
+
+        //felt 16
+        gui.getFields()[16].setTitle("Legetøjsbutikken");
+        gui.getFields()[16].setSubText("M3");
+        gui.getFields()[16].setForeGroundColor(Color.BLACK);
+        gui.getFields()[16].setBackGroundColor(Color.WHITE);
+        gui.getFields()[16].setDescription("Du landede på legetøjsbutikken");
+
+        //felt 17
+        gui.getFields()[17].setTitle("Dyrehandlen");
+        gui.getFields()[17].setSubText("M3");
+        gui.getFields()[17].setForeGroundColor(Color.BLACK);
+        gui.getFields()[17].setBackGroundColor(Color.WHITE);
+        gui.getFields()[17].setDescription("Du landede på dyrehandlen");
+
+        //felt 18
+        gui.getFields()[18].setTitle("Gå i fængsel");
+        gui.getFields()[18].setSubText("");
+        gui.getFields()[18].setForeGroundColor(Color.BLACK);
+        gui.getFields()[18].setBackGroundColor(Color.LIGHT_GRAY);
+        gui.getFields()[18].setDescription("Ryk til fængslet");
+
+        //felt 19
+        gui.getFields()[19].setTitle("Bowlinghallen");
+        gui.getFields()[19].setSubText("M4");
+        gui.getFields()[19].setForeGroundColor(Color.BLACK);
+        gui.getFields()[19].setBackGroundColor(Color.MAGENTA);
+        gui.getFields()[19].setDescription("Du landede på bowlinghallen");
+
+        //felt 20
+        gui.getFields()[20].setTitle("Zoo");
+        gui.getFields()[20].setSubText("M4");
+        gui.getFields()[20].setForeGroundColor(Color.BLACK);
+        gui.getFields()[20].setBackGroundColor(Color.MAGENTA);
+        gui.getFields()[20].setDescription("Du landede på Zoo");
+
+        //felt 21
+        gui.getFields()[21].setTitle("Prøv Lykken");
+        gui.getFields()[21].setSubText("");
+        gui.getFields()[21].setForeGroundColor(Color.BLACK);
+        gui.getFields()[21].setBackGroundColor(Color.ORANGE);
+        gui.getFields()[21].setDescription("Prøv lykken kort");
+
+        //felt 22
+        gui.getFields()[22].setTitle("Vandlandet");
+        gui.getFields()[22].setSubText("M5");
+        gui.getFields()[22].setForeGroundColor(Color.BLACK);
+        gui.getFields()[22].setBackGroundColor(Color.CYAN);
+        gui.getFields()[22].setDescription("Du landede på vandlandet");
+
+        //felt 23
+        gui.getFields()[23].setTitle("Strandpromenaden");
+        gui.getFields()[23].setSubText("M5");
+        gui.getFields()[23].setForeGroundColor(Color.BLACK);
+        gui.getFields()[23].setBackGroundColor(Color.CYAN);
+        gui.getFields()[23].setDescription("Du landede på strandpromenaden");
+
+        gui.setDice(1, 2);
+    }
 }
+
 
 
 
