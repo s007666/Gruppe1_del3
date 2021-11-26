4367a5d0f245adb507ba6cff19df6189fa01fd75
Mohamad Meri <moraimohamed7@gmail.com>
2021-11-26 08:35:24 +0100

    Sidste commit 27.

--- src/main/java/com/company/FlytBil.java
+++ src/main/java/com/company/FlytBil.java
@@ -4,7 +4,6 @@ import gui_fields.GUI_Field;
 import gui_fields.GUI_Player;
 import gui_main.GUI;
 
-import java.lang.reflect.Field;
 
 public class FlytBil {
 
@@ -19,24 +18,4 @@ public class FlytBil {
     public void flyt(GUI_Player spiller){
         field.setCar(spiller, true);
     }
-
-
-
 }
-
-
-
-  /*  private static GUI move_instance = null;
-    public static GUI instance() {
-        if (move_instance == null) {
-            move_instance = new GUI();
-        }
-        return move_instance;
-    }
-// Sætter spillerens bil på start
-     
-// Forkert: Nu vises spillerns bil på begge felter!
-
-// Sådan, nu er bilen fjernet fra felt
-
-    }*/
